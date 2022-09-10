efe15417068e0721ffdd3c2df4efdb116453e1ba
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-09-10 13:30:34 -0400

    Update project.py

--- src/utils/projectsmanagement/project.py
+++ src/utils/projectsmanagement/project.py
@@ -1,13 +1,29 @@
-from os import mkdir
+from os import mkdir, isfile
 from ..filemanagement import isdir, get_path, lstdir, get_path_str 
 class project:
   def __new__(self, ProjectName: str):
-    if (self.path := isdir(self.path)) is False: raise ValueError("No such project")
+    if isdir((self.path := get_path)) is False: raise ValueError("No such project")
     self.name = ProjectName
     self.files = lstdir(ProjectName)
     self.path_str = get_path_str(ProjectName)
     return self
 
+  def run(self, File: str, *, endfunc = None):
+    '''
+  Run a translated tython code
+  '''
+  FileName = f'{self.path_str}/{File}'
+  if isfile(FileName) is False: raise ValueError('No such file')
+  p = subprocess.Popen(f'python3 {FileName}', shell=True)
+  out, err = p.communicate()
+  print(f"Code exited.\nSubprocess Ouput : {out}\nSubprocess error : {err}")
+  if endfunc is None:
+    if out is not None or err is not None:
+      return {"out": out, "error": err}
+    return None
+  endfunc(out, err)
+  return None
+  
   def add_file(self, Name: str, Content: str):
     with open(f'{self.path_str}/{Name}', 'x') as f:
       f.write(Content)
