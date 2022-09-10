4b82653cb0ad3edf77b942a1dfa2d40e8fa503f4
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-09-10 13:27:30 -0400

    Create process.py

---
+++ src/run/process.py
@@ -0,0 +1,22 @@
+from ..utils import
+import subprocess
+from typing import Optional
+project_path = __file__[: -(len((e := __file__.split('/'))[-1]) + len(e[-2]))] + '/data/run'
+def run(Code: str, FileName: str = 'Runner.ty', *, endfunc = None) -> Optional[dict]:
+  '''
+  Run a translated tython code
+  '''
+  FileName = f"{project_path.replace('/', '.')}/{run}"
+  with open(FileName, 'w') as f:
+    f.write(Code)
+  p = subprocess.Popen(f'python3 {FileName}', shell=True)
+  out, err = p.communicate()
+  print(f"Code exited.\nSubprocess Ouput : {out}\nSubprocess error : {err}")
+  if endfunc is None:
+    if out is not None or err is not None:
+      return {"out": out, "error": err}
+    return None
+  endfunc(out, err)
+  return None
+  
+  
