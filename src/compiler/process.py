3a9a265bab4630b9d01592fe252e531e488c57ce
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-09-10 12:17:31 -0400

    Create process.py

---
+++ src/compiler/process.py
@@ -0,0 +1,43 @@
+from .data import data
+from ..run import run
+def reset_indent(content: str) -> str:
+  e = 0
+  for i in content:
+    if i != ' ':
+      e += 1
+  return content[e:]
+
+def frst(Content: str) -> str:
+  return Content[0] + Content[1] + Content[2] + Content[3]
+
+class indentation:
+  def __init__(self):
+    self.indent: int = 0
+
+  def __str__(self):
+    e: str = ''
+    for i in range(self.indent):
+      e += '\t'
+    return e
+
+def compile(File: str, /, *, CompiledPlace: str = None, autorun: bool = True) -> None:
+  '''
+  Compile a ty file into python code
+  '''
+  indent = indentation()
+  with open(File, 'r') as f:
+    content = r.read()
+  py: str = ''
+  for line in content:
+    line = reset_indent(line)
+    try:
+      nl, indent = data[frst(line)](line, indent)
+    except KeyError:
+      nl += line
+    py += f'{indent}{nl}\n'
+  if CompiledPlace is not None:
+    with open(CompiledPlace, 'w') as f:
+      f.write(py)
+  if autorun is True:
+    run(py, File)
+  return py
